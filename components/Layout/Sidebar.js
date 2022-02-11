8f463eb46bd460f4ce42115603dbc6ba77d4cf69
zhalok <zhalokrahman007@gmail.com>
2022-02-11 09:45:37 +0600

    .

--- components/Layout/Sidebar.js
+++ components/Layout/Sidebar.js
@@ -1,12 +1,7 @@
 import { useEffect, useState } from 'react';
-import OutlineButton from '../Buttons/Outlined';
-import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
-import ClassTwoToneIcon from '@mui/icons-material/ClassTwoTone';
-import CoPresentTwoToneIcon from '@mui/icons-material/CoPresentTwoTone';
-import PeopleTwoToneIcon from '@mui/icons-material/PeopleTwoTone';
 import { useRouter } from 'next/router';
-import LoginIcon from '@mui/icons-material/Login';
-import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import TeacherSidebar from './TeacherSidebar';
+import StudentSidebar from './StudentSidebar';
 
 export default function Sidebar() {
 	const [windowWidth, setWindowWidth] = useState(0);
@@ -37,68 +32,58 @@ export default function Sidebar() {
 			.catch((e) => console.log(e));
 	}, []);
 
+	// console.log(loggedInUser);
+	if (loggedInUser && loggedInUser.role == 'teacher')
+		return <TeacherSidebar loggedInUser={loggedInUser} />;
+	if (loggedInUser && loggedInUser.role == 'student')
+		return <StudentSidebar loggedInUser={loggedInUser} />;
+
 	return (
-		<div
-			style={{
-				height: windowHeight,
-				border: '1px solid black',
-				borderRadius: '10px',
-			}}
-		>
+		<div className='sidebar'>
 			<div
-				style={{
-					marginTop: '100px',
-					marginLeft: 'auto',
-					marginRight: 'auto',
+				className='sidebar-elements'
+				style={{ marginTop: '100px' }}
+				onClick={() => {
+					router.push('/');
 				}}
 			>
-				<div
-					style={{
-						border: '1px solid black',
-						padding: '20px',
-						width: '60%',
-						textAlign: 'center',
-						marginLeft: 'auto',
-						marginRight: 'auto',
-					}}
-					onClick={() => {
-						router.push('/');
-					}}
-				>
-					Home
-				</div>
-				<div
-					style={{
-						border: '1px solid black',
-						padding: '20px',
-						width: '60%',
-						textAlign: 'center',
-						marginLeft: 'auto',
-						marginRight: 'auto',
-						marginTop: '10px',
-					}}
-					onClick={() => {
-						router.push('/courses');
-					}}
-				>
-					Courses
-				</div>
-				<div
-					style={{
-						border: '1px solid black',
-						padding: '20px',
-						width: '60%',
-						textAlign: 'center',
-						marginLeft: 'auto',
-						marginRight: 'auto',
-						marginTop: '10px',
-					}}
-					onClick={() => {
-						router.push('/teachers');
-					}}
-				>
-					Mentors
-				</div>
+				Home
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push('/courses');
+				}}
+			>
+				Courses
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push('/teachers');
+				}}
+			>
+				Mentors
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push('/login');
+				}}
+			>
+				Log in
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push('/signup');
+				}}
+			>
+				Sign up
 			</div>
 		</div>
 	);
--- components/Layout/TeacherSidebar.js
+++ components/Layout/TeacherSidebar.js
@@ -1,3 +1,63 @@
-export default function TeacherSidebar() {
-	return <div className='sidebar'>TeacherSidebar</div>;
+import { useEffect, useState } from 'react';
+import OutlineButton from '../Buttons/Outlined';
+import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
+import ClassTwoToneIcon from '@mui/icons-material/ClassTwoTone';
+import CoPresentTwoToneIcon from '@mui/icons-material/CoPresentTwoTone';
+import PeopleTwoToneIcon from '@mui/icons-material/PeopleTwoTone';
+import { useRouter } from 'next/router';
+import LoginIcon from '@mui/icons-material/Login';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+
+export default function StudentSidebar({ loggedInUser }) {
+	const router = useRouter();
+	return (
+		<div className='sidebar'>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '100px' }}
+				onClick={() => {
+					router.push('/');
+				}}
+			>
+				My courses
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push('/courses');
+				}}
+			>
+				My classrooms
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push(`/studentlist/${loggedInUser.userId}`);
+				}}
+			>
+				My students
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					localStorage.removeItem('token');
+					router.push('/');
+				}}
+			>
+				Log out
+			</div>
+			<div
+				className='sidebar-elements'
+				style={{ marginTop: '10px' }}
+				onClick={() => {
+					router.push(`/profile/${loggedInUser.userId}`);
+				}}
+			>
+				Profile
+			</div>
+		</div>
+	);
 }
